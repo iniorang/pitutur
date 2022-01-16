a495210efb118031972f37b1f50197791b3cd0e3
ini_orang <freemiumlemper300@gmail.com>
2022-01-16 15:15:05 +0700

    Init commit

---
+++ app/src/main/java/com/faqihapp/pitutur/splashscreen_act.java
@@ -0,0 +1,16 @@
+package com.faqihapp.pitutur;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+@SuppressLint("CustomSplashScreen")
+public class splashscreen_act extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen_act);
+    }
+}
