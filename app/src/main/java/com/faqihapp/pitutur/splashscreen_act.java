369326fb14a3ffbe6c987707ae01c6dea248c94b
ini_orang <freemiumlemper300@gmail.com>
2022-02-14 13:34:10 +0700

    Add Login

--- app/src/main/java/com/faqihapp/pitutur/splashscreen_act.java
+++ app/src/main/java/com/faqihapp/pitutur/splashscreen_act.java
@@ -3,7 +3,12 @@ package com.faqihapp.pitutur;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.window.SplashScreen;
 
 @SuppressLint("CustomSplashScreen")
 public class splashscreen_act extends AppCompatActivity {
@@ -12,5 +17,13 @@ public class splashscreen_act extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splashscreen_act);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(splashscreen_act.this, Login.class));
+                finish();
+            }
+        },2000);
     }
 }
